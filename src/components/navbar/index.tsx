70c00ff84518351a5e0d2e49f67d4692dfac284d
subhash131 <subhashnayak131@gmail.com>
2024-12-04 23:27:40 +0530

    add navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+import { RiLeafFill } from "react-icons/ri";
+
+const navItems = [
+  {
+    name: "How it works",
+    route: "#",
+  },
+  {
+    name: "Benefits",
+    route: "#",
+  },
+  {
+    name: "Services",
+    route: "#",
+  },
+  {
+    name: "Pricing",
+    route: "#",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-fit flex items-center justify-center sticky top-0">
+      <div className="w-[80%] h-12 flex justify-between items-center">
+        <div className="flex items-center gap-1 drop-shadow-md">
+          <RiLeafFill />
+          <h1 className="font-semibold">Creatify</h1>
+        </div>
+        <div className="flex rounded-lg  backdrop-blur-md">
+          {navItems.map(({ name, route }) => {
+            return (
+              <Link
+                href={route}
+                className="px-2 py-1 hover:bg-gray-200 rounded-lg text-sm"
+              >
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+        <button className="bg-[#7C4BFF] border border-[#7C4BFF] hover:border-white transition-colors text-white px-4 py-1 rounded-lg shadow-sm shadow-[#7C4BFF]">
+          Register
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
