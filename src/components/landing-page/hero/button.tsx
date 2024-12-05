de2e44aa9e5829c35c5663f358d28201280c595e
subhash131 <subhashnayak131@gmail.com>
2024-12-06 01:53:20 +0530

    fix css

--- src/components/landing-page/hero/button.tsx
+++ src/components/landing-page/hero/button.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { en } from "@/components/localization/en";
+import Link from "next/link";
 
 const lang = en;
 
 const LoginButton = () => {
   return (
-    <button className="px-10 py-3 bg-black rounded-xl shadow-md text-white hover:scale-[.99] transition-transform">
+    <Link
+      href="#"
+      className="px-10 py-3 bg-black rounded-xl shadow-md text-white hover:scale-[.99] transition-transform max-sm:px-8 max-sm:py-2"
+    >
       {lang.hero.getStartedButton}
-    </button>
+    </Link>
   );
 };
 
--- src/components/landing-page/hero/key-features.tsx
+++ src/components/landing-page/hero/key-features.tsx
@@ -6,7 +6,7 @@ const lang = en;
 
 const KeyFeatures = () => {
   return (
-    <div className="w-full flex gap-4 justify-center max-md:text-xs max-sm:flex-col items-center">
+    <div className="w-full flex gap-4 justify-center max-md:text-xs max-sm:flex-col items-center max-sm:gap-1">
       {lang.hero.highlights.map((feature) => {
         return (
           <div className="flex items-center gap-1" key={`key-${feature}`}>
